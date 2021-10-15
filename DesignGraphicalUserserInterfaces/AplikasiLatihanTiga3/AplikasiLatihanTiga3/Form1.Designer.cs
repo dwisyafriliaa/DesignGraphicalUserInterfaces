ebc31b5d79ae5ed72f2cbeac6c32b2ae89f29c7e
dwisyafriliaa <92431521+dwisyafriliaa@users.noreply.github.com>
2021-10-15 12:44:50 +0700

    Add files via upload

---
+++ DesignGraphicalUserserInterfaces/AplikasiLatihanTiga3/AplikasiLatihanTiga3/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace AplikasiLatihanTiga3
+{
+    partial class frmLatihanTiga3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.cmbPendidikan = new System.Windows.Forms.ComboBox();
+            this.lstPekerjaan = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(96, 27);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(355, 20);
+            this.txtNama.TabIndex = 1;
+            // 
+            // cmbPendidikan
+            // 
+            this.cmbPendidikan.FormattingEnabled = true;
+            this.cmbPendidikan.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "Pascasarjana"});
+            this.cmbPendidikan.Location = new System.Drawing.Point(96, 60);
+            this.cmbPendidikan.Name = "cmbPendidikan";
+            this.cmbPendidikan.Size = new System.Drawing.Size(355, 21);
+            this.cmbPendidikan.TabIndex = 2;
+            // 
+            // lstPekerjaan
+            // 
+            this.lstPekerjaan.FormattingEnabled = true;
+            this.lstPekerjaan.Items.AddRange(new object[] {
+            "Pedagang",
+            "PNS",
+            "Dosen",
+            "Swasta",
+            "ABRI"});
+            this.lstPekerjaan.Location = new System.Drawing.Point(96, 95);
+            this.lstPekerjaan.Name = "lstPekerjaan";
+            this.lstPekerjaan.Size = new System.Drawing.Size(355, 95);
+            this.lstPekerjaan.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Pendidikan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Pekerjaan";
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.Enabled = false;
+            this.txtPesan1.Location = new System.Drawing.Point(15, 208);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(436, 20);
+            this.txtPesan1.TabIndex = 1;
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.Enabled = false;
+            this.txtPesan2.Location = new System.Drawing.Point(15, 234);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(436, 20);
+            this.txtPesan2.TabIndex = 1;
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.Enabled = false;
+            this.txtPesan3.Location = new System.Drawing.Point(15, 260);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(436, 20);
+            this.txtPesan3.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(15, 286);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(436, 22);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // frmLatihanTiga3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 329);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lstPekerjaan);
+            this.Controls.Add(this.cmbPendidikan);
+            this.Controls.Add(this.txtPesan3);
+            this.Controls.Add(this.txtPesan2);
+            this.Controls.Add(this.txtPesan1);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLatihanTiga3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan 3.3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.ComboBox cmbPendidikan;
+        private System.Windows.Forms.ListBox lstPekerjaan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan3;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ DesignGraphicalUserserInterfaces/AplikasiSelamatDatang/AplikasiSelamatDatang/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiSelamatDatang
+{
+    public partial class frmSelamatDatang : Form
+    {
+        public frmSelamatDatang()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblKeterangan.Text = "Selamat Datang Di Pemrograman C#";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblKeterangan.Text = "Begitu mudahnya membuat aplikasi berbasis windows";
+        }
+    }
+}
